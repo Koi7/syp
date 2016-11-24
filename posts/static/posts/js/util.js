dcfef45dc99c1f3d439e921e76b3a4ff9da7af15
Ramazan <shunen666@gmail.com>
2016-11-24 18:02:39 +0300

    Fixing ajax integration

---
+++ posts/static/posts/js/util.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ramazan on 24.11.2016.
+ */
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}

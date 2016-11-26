aee2f2558f272b6c95ed569dafe29dfcf9c924af
Ramazan <shunen666@gmail.com>
2016-11-26 14:06:11 +0300

    work on auto-update user's profile on login

--- posts/models.py
+++ posts/models.py
@@ -26,10 +26,10 @@ def save_user_profile(sender, instance, **kwargs):
     instance.vkuser.save()
 
 @receiver(user_logged_in, sender=User)
-def update_user_profile(sender, **kwargs):
-    vk_api_request_url = "https://api.vk.com/method/users.get?user_ids=" + sender.username + "&fields=photo_50&v=5.60"
+def update_user_profile(sender, instance, **kwargs):
+    vk_api_request_url = "https://api.vk.com/method/users.get?user_ids=" + instance.username + "&fields=photo_50&v=5.60"
     json = urlopen(vk_api_request_url).read()
-    sender.vkuser.photo_rec = json[0]['photo_50']
+    instance.vkuser.photo_rec = json[0]['photo_50']
     sender.save()
 
 class HashBackend(object):

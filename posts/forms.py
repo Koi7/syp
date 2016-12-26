3301cdc8a23344a2616b7c42de7aa2c67fc9420e
Ramazan <izmaylov.ramazan@yandex.ru>
2016-12-26 18:19:02 +0300

    working on post model

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django import forms
 class PostForm(forms.Form):
-    text = forms.CharField(label=u'Текст', widget=forms.Textarea, max_length=2000)
-    is_anonymous = forms.BooleanField(label=u'Анонимно?', widget=forms.CheckboxInput(check_test=True))
+    text = forms.CharField(label='Текст', widget=forms.Textarea, max_length=2000)
+    is_anonymous = forms.BooleanField(label='Анонимно?', widget=forms.CheckboxInput(check_test=True))

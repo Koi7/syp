aa524aad4db6365b4304738e4887b11cc9897787
Ramazan <izmaylov.ramazan@yandex.ru>
2016-12-26 21:07:09 +0300

    working on post model

--- posts/forms.py
+++ posts/forms.py
@@ -3,4 +3,5 @@
 from django import forms
 class PostForm(forms.Form):
     text = forms.CharField(label='Текст', widget=forms.Textarea, max_length=2000)
-    is_anonymous = forms.BooleanField(label='Анонимно?', widget=forms.CheckboxInput(check_test=True))
+    is_anonymous = forms.BooleanField(required=False)
+    

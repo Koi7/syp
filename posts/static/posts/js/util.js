905bca3b0f09f11d68ee675257b9f3b172ebb254
Ramazan <izmaylov.ramazan@yandex.ru>
2016-12-29 18:53:57 +0300

    working on users location autodetection

--- posts/static/posts/js/util.js
+++ posts/static/posts/js/util.js
@@ -9,7 +9,46 @@ function getCookie(name) {
 
 function getPlace(city) {
   var service = new google.maps.places.AutocompleteService();
-  service.getQueryPredictions({ input: 'Феодосия' }, function (array, status) {
+  service.getQueryPredictions({ input: city }, function (array, status) {
     if (status != google.maps.places.PlacesServiceStatus.OK) return city; else return array[0].description;
   });
+}
+
+function getDataAndRedirect() {
+  VK.Widgets.Auth("vk_auth", {width: "200px", onAuth: function(data) {
+              $.ajax({
+              type: 'POST',
+              url: 'login',
+              data: {
+                  uid: data.uid,
+                  first_name: data.first_name,
+                  last_name: data.last_name,
+                  photo_rec: data.photo_rec,
+                  place: getPlace(getCity(data.uid)),
+                  hash: data.hash,
+                  csrfmiddlewaretoken: getCookie('csrftoken')
+              },
+              success: function(response){
+                    if (response['success']){
+                        window.location = 'posts';
+                    }
+              },
+              dataType: 'json'
+          });
+          } });
+}
+
+function getCity(uid){
+  city = '';
+  $.ajax({
+              type: 'GET',
+              url: 'https://api.vk.com/method/users.get?user_ids=' + uid + '&fields=city&v=5.60',
+              success: function(response){
+                    response.forEach(function (item, i, arr) {
+                        city = item['city']['title'];
+                    })
+              },
+              dataType: 'json'
+          });
+  return city;
 }

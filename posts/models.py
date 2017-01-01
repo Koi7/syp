01bae2b62c50c1b66979e78346c0dd317eabde65
Ramazan <izmaylov.ramazan@yandex.ru>
2017-01-01 14:33:27 +0300

    place autodetection

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.signals import user_logged_in
 import hashlib
+import json
+import requests
 
 #Create your models here.
 
@@ -31,27 +33,18 @@ class VKUser(models.Model):
     @receiver(user_logged_in, sender=User)
     def update_user_profile(user, **kwargs):
         if not user.is_superuser:
-            vk_api_method = 'users.get'
-            vk_api_parameter_names = iter(['user_ids', 'fields', 'v'])
-            vk_api_parameters = iter([user.username, 'photo_50,first_name,last_name,city', '5.60'])
-            vk_api_request_url = '%s%s?%s=%s&%s=%s&%s=%s' % (settings.VK_API_URL,
-                                                             vk_api_method,
-                                                             next(vk_api_parameter_names),
-                                                             next(vk_api_parameters),
-                                                             next(vk_api_parameter_names),
-                                                             next(vk_api_parameters),
-                                                             next(vk_api_parameter_names),
-                                                             next(vk_api_parameters))
-            import json
-            import requests
-            response = requests.get(settings.VK_API_URL, params={'v': '5.60', 'lang': settings.LANGUAGE_CODE[0:2], 'fields': 'photo_50,first_name,last_name,city', 'user_ids': user.username})
-            data = json.loads(response.text)
-            for user_data in data['response']:
+            response = requests.get(settings.VK_API_URL, params={'v': '5.60',
+                                                                 'lang': settings.LANGUAGE_CODE[0:2],
+                                                                 'fields': 'photo_50,first_name,last_name,city',
+                                                                 'user_ids': user.username})
+            for user_data in json.loads(response.text)['response']:
                 user.vkuser.photo_rec = user_data['photo_50']
                 user.first_name = user_data['first_name']
                 user.last_name = user_data['last_name']
                 #request to google places api
-                response = requests.get(settings.GOOGLE_API_URL, params={'input': user_data['city']['title'], 'language': settings.LANGUAGE_CODE[0:2], 'key': settings.GOOGLE_PLACES_API_KEY})
+                response = requests.get(settings.GOOGLE_API_URL, params={'input': user_data['city']['title'],
+                                                                         'language': settings.LANGUAGE_CODE[0:2],
+                                                                         'key': settings.GOOGLE_PLACES_API_KEY})
                 user.vkuser.place = json.loads(response.text)['predictions'][0]['description']
                 user.vkuser.save()
 

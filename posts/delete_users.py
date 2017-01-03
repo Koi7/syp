40f167e26a222d957a1d4765f578863d3cf65887
Ramazan <izmaylov.ramazan@yandex.ru>
2017-01-03 19:44:19 +0300

    trying to add custom manage.py command

--- posts/delete_users.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-
-all_users = User.objects.all()
-for user in all_users:
-    user.delete()
---
+++ posts/managment/commands/deleteusers.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Deletes all users in database'
+
+    def handle(self, *args, **options):
+        all_users = User.objects.all()
+        for user in all_users:
+            user.delete()

78de0fd8a788279d3dacae0682efe855b84586d1
Ramazan <izmaylov.ramazan@yandex.ru>
2017-01-03 23:54:10 +0300

    user must specify place before auth

--- posts/static/posts/js/autocomplete.js
+++ posts/static/posts/js/autocomplete.js
@@ -11,27 +11,7 @@
 
 		google.maps.event.addListener(autocomplete, 'place_changed', function() {
 		    var place = autocomplete.getPlace();
-			getWeatherByCity(place.name);
+			getDataAndRedirect(place.name);
 	    });
 	}
 	google.maps.event.addDomListener(window, 'load', init);
-
-	var getWeatherByCity = function (city) {
-		/*var httpRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-		httpRequest.onreadystatechange = function () {
-			var jsonText = httpRequest.readyState == 4 && httpRequest.status == 200 ? httpRequest.responseText : null;
-			var weather = JSON.parse(jsonText);
-			if (weather.cod == 404) {
-				alert("= ( city not found");
-				return;
-			}
-			var tempView = document.getElementById("temp");
-			var tempValue = Math.floor(KtoC(weather.main.temp));
-			tempView.innerHTML = tempValue + " C&deg;";
-			showThermometer();
-			moveRuth(tempValue);
-		}
-		httpRequest.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city, true);
-		httpRequest.send();*/
-		alert(city);
-	}

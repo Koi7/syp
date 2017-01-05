234de834f3b38b859b9235a3c1e675e8f017b81f
Koi7 <shunen666@gmail.com>
2017-01-05 14:50:02 +0300

    add pythonanywhere to allowed hosts

--- posts/views.py
+++ posts/views.py
@@ -67,25 +67,6 @@ def add_post(request):
         return redirect('posts')
 
 @login_required
-def delete_post(request):
-    Post.objects.get(id=request.POST.get('post_id')).delete()
-    return redirect('posts')
-def edit_post(request, post_id):
-    if request.method == 'GET':
-        post_to_edit = Post.objects.get(id=post_id)
-        context = {
-            'text': post_to_edit.text,
-            'is_anonymous': 'on' if post_to_edit.is_anonymous else 'off',
-        }
-        return render(request, 'posts/edit_post.html', context)
-    if request.method == 'POST':
-        post_to_edit =  Post.objects.get(id=post_id)
-        post_to_edit.text = request.POST.get('text')
-        post_to_edit.is_anonymous = True if request.POST.get('is_anonymous') == 'on' else False
-        post_to_edit.save()
-        return  redirect('posts')
-
-@login_required
 def posts(request):
     """
         View for default page of logged user.

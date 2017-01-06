44d901f722f12e0b8957252f9c29817fcda0e2a8
Koi7 <shunen666@gmail.com>
2017-01-06 23:09:45 +0300

    making site

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
     url(r'^add', views.add_post, name='add_post'),
-    url(r'^edit', views.edit_post, name='edit_post'),
+    url(r'^edit$', views.edit_post, name='edit_post'),
     url(r'^delete', views.delete_post, name='delete_post'),
     url(r'^make_not_relevant', views.make_post_not_relevant, name='make_post_not_relevant'),
     url(r'^like', views.like_post, name='like_post'),

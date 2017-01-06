232351b69c4fb8e9e439159c9429f5cf778c87a5
Koi7 <shunen666@gmail.com>
2017-01-06 23:31:05 +0300

    making site

--- posts/urls.py
+++ posts/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
     url(r'^add', views.add_post, name='add_post'),
-    url(r'^edit/?(<post_id>[0-9]]+)/$', views.edit_post, name='edit_post'),
+    url(r'^edit/(?P<post_id>[0-9]]+)/$', views.edit_post, name='edit_post'),
     url(r'^save', views.save_editions, name='save_editions'),
     url(r'^delete', views.delete_post, name='delete_post'),
     url(r'^make_not_relevant', views.make_post_not_relevant, name='make_post_not_relevant'),
     url(r'^like', views.like_post, name='like_post'),
-    url(r'^liked/?(<post_id>[0-9]]+)/$', views.liked, name='liked'),
+    url(r'^liked/(?P<post_id>[0-9]]+)/$', views.liked, name='liked'),
 ]

8159899ef8688408736e2264fc12c628239eaf25
Koi7 <shunen666@gmail.com>
2017-01-07 00:10:13 +0300

    ;)

--- posts/urls.py
+++ posts/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
-    url(r'^add', views.add_post, name='add_post'),
+    url(r'^add$', views.add_post, name='add_post'),
     url(r'^edit/(?P<post_id>[0-9]+)/$', views.edit_post, name='edit_post'),
-    url(r'^save', views.save_editions, name='save_editions'),
-    url(r'^delete', views.delete_post, name='delete_post'),
-    url(r'^make_not_relevant', views.make_post_not_relevant, name='make_post_not_relevant'),
+    url(r'^save$', views.save_editions, name='save_editions'),
+    url(r'^delete$', views.delete_post, name='delete_post'),
+    url(r'^make_not_relevant$', views.make_post_not_relevant, name='make_post_not_relevant'),
     url(r'^like$', views.like_post, name='like_post'),
     url(r'^liked/(?P<post_id>[0-9]+)/$', views.liked, name='liked'),
 ]

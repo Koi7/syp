d386c854f502fe7709d062c609bbb26a980a14fa
Koi7 <shunen666@gmail.com>
2017-05-20 11:54:04 +0300

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 // gulp and plugins
-var gulp = require('gulp'),
-	run = require('gulp-run');
+var gulp = require('gulp')
 
 // other
 var ip = require('quick-local-ip');
+var spawn = require('child_process').spawn;
 
-// rasks
-gulp.task('start', function(){
-	var local_ip = ip.getLocalIP4();
-	var port = ':8000';
-	return run('python manage.py runserver ' + local_ip + port).exec()    // prints "Hello World\n". 
-    .pipe(gulp.dest('output'))      // writes "Hello World\n" to output/echo. 
-  	;
+// tasks
+gulp.task('start', function() {
+    var runserver = spawn(
+        'python',
+        ['manage.py', 'runserver', ip.getLocalIP4() + ':8000'], 
+        { stdio: 'inherit' }
+    );
+    
+    runserver.on('close', function(code) {
+        if (code !== 0) {
+            console.error('Django runserver exited with error code: ' + code);
+        } else {
+            console.log('Django runserver exited normally.');
+        }
+    });
 });
+
+
